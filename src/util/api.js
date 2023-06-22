85346d67696fa53ac2e3e16060f8415f6b2fb23c
WarrenLynes <warrenlynes@gmail.com>
2023-06-22 16:50:07 -0600

    Product Overview layout - created simple layout in ProductOverview - created api.js

---
+++ src/util/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const baseURL = process.env.API_URL;
+const apiKey = process.env.API_KEY;
+
+function executeRequest(endpoint, method = 'get', headers = {}, body = {}) {
+  const url = baseURL + endpoint;
+  headers = {
+    Authorization: apiKey,
+    ...headers
+  };
+
+  return axios({
+    url,
+    method,
+    headers,
+    body
+  }).then(({ data }) => data)
+    .catch(err => {
+      throw Error(err);
+    });
+}
+
+export async function fetchProductRatings(productId) {
+  const endpoint = '/reviews/meta?product_id=' + productId;
+
+  try {
+    return await executeRequest(endpoint, 'get');;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export async function fetchProduct(productId) {
+  const endpoint = '/products/' + productId;
+
+  try {
+    const product = await executeRequest(endpoint, 'get');
+    const styles = await executeRequest(endpoint + '/styles');
+    const rating = await fetchProductRatings(productId);
+    return { ...product, styles: styles.results, rating };
+  } catch (err) {
+    console.error(err);
+  }
+
+}
