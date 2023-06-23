8326b861469dfe79b2c8ac91ed0e04a699995376
victoriajquinto <victoriajquinto@gmail.com>
2023-06-22 19:16:14 -0700

    add related redux state to related component

--- src/state/index.js
+++ src/state/index.js
@@ -2,13 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import AppReducer from './app/reducer.js';
 import ProductDetailReducer from './productDetail/reducer.js';
+import relatedReducer from './related/reducer.js';
 
 const rootReducer = combineReducers({
   app: AppReducer,
-  productDetail: ProductDetailReducer
+  productDetail: ProductDetailReducer,
+  related: relatedReducer
 });
 
 export const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
-)
+);
