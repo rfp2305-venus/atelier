4f1169c30345fc7a7183d0530ff888f6e848766e
WarrenLynes <warrenlynes@gmail.com>
2023-06-26 10:06:25 -0600

    ProductGallery mobile first

--- src/components/ProductOverview/ProductGallery.js
+++ src/components/ProductOverview/ProductGallery.js
@@ -1,10 +1,29 @@
 
-import React from "react";
+import React, {useEffect} from "react";
+
+function renderMobile() {
+  return (
+    <div className="img-row" style={{height: '35vh', maxWidth: '100%', display: 'flex', overflow: 'scroll'}}>
+      {product.photos.map((photo) => (
+        <img src={photo.url} alt="" style={{maxHeight: '50vh'}}/>
+      ))}
+    </div>
+  );
+}
 
 export default function ProductGallery({product}) {
-
   console.log(product)
+
+  useEffect(() => {
+    console.log(window.innerWidth);
+
+  }, [window.innerWidth]);
+
   return (
-    <img src={product.photos[0].url} alt="" style={{maxHeight: '50vh'}}/>
+    <div className="img-row" style={{height: '35vh', maxWidth: '100%', display: 'flex', overflow: 'scroll'}}>
+      {product.photos.map((photo) => (
+        <img src={photo.url} alt="" style={{maxHeight: '50vh'}}/>
+      ))}
+    </div>
   );
 }
