a72da45e1211127fc7aa1fcc0f33a08dec2265b1
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-06-26 11:49:08 -0500

    Mounted Q&A comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,26 @@ import { useSelector } from 'react-redux';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
 import RelatedComp from './relatedAndComp/RelatedComp.jsx';
 
+import QuestionsList from './qAndA/QuestionsList';
+
 export default function App() {
   const { loading } = useSelector(({ app }) => app);
 
-  console.log(loading);
+  // console.log(loading);
   return (
     <>
       <div id="app">
         {loading ? <h2>loading</h2> : null}
         <ProductOverview />
+
+
+        <QuestionsList />
+
+
       </div>
       <div>
         <RelatedComp />
+
       </div>
     </>
   );
