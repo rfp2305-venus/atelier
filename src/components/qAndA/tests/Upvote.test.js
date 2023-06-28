517cfd0ab9b1e1d3c9c5e57333ff89c460b1aeca
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-06-28 02:54:57 -0500

    Notes added

--- src/components/qAndA/tests/Upvote.test.js
+++ src/components/qAndA/tests/Upvote.test.js
@@ -10,15 +10,20 @@ import axios from 'axios';
 
 import Upvote from '../Upvote';
 
+// mock axios —> custom behavior for API req
 jest.mock('axios');
 
 describe('Upvote component', () => {
 
   test('increments helpfulness & disables appropriately on click', async() => {
 
+    // simulate successful res from axios.put
     const res = { status: 200 };
+
+    // instruct mock func to return res (as resolved promise) when called
     axios.put.mockResolvedValue(res);
 
+    // render Upvote component w/ necessary props
     const { getByText } = render(
       <Upvote
         id={ 1 }
@@ -27,12 +32,18 @@ describe('Upvote component', () => {
       />
     );
 
+    // query DOM elem by given text
     const upvoteButton = getByText('Yes (0)');
 
+    // trigger click event
     fireEvent.click(upvoteButton);
 
+    // wait for axios req before assertions
     await waitFor(() => {
+
+      // verify expected behaviors
       expect(axios.put).toHaveBeenCalledTimes(1);
+
       expect(axios.put).toHaveBeenCalledWith(
         `${ API_URL }/qa/questions/1/helpful`,
         { helpfulness: 1 },
