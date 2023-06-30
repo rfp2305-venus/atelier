1cf58f0b4c4f11d4d685f9a076f56d6db7ace8e1
victoriajquinto <victoriajquinto@gmail.com>
2023-06-30 12:51:21 -0700

    stub functions for outfit in YourOutfit component

--- src/components/relatedAndComp/YourOutfit.jsx
+++ src/components/relatedAndComp/YourOutfit.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable func-style */
+import react from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import OutfitCard from './OutfitCard';
 import AddToOutfit from './AddToOutfit';
 import { Box, Typography } from '@mui/material';
 
-
 export default function YourOutfit() {
+  const [ outfit, setOutfit ] = useState([]);
+
+  //declare add to outfit fn - pass into AddToOutfit prop
+  function wearArticle() {
+
+  }
+  //declare remove from outfit fn - pass into each OutfitCard
+  function removeArticle() {
+    
+  }
+
   return (
     <>
       <Typography component='p' className="title">YOUR OUTFIT</Typography>
