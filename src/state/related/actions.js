747d1f585189d78526abc46b19ddd994f949c2a2
victoriajquinto <victoriajquinto@gmail.com>
2023-06-29 19:23:34 -0700

    create new action for comparison product reducer

--- src/state/related/actions.js
+++ src/state/related/actions.js
@@ -7,14 +7,28 @@
 //   };
 // }
 
+import { fetchProduct } from '../../util/api.js';
+import { handleSetLoading } from '../app/actions.js';
 
+export const COMPARISON = 'COMPARISON_PRODUCT';
 
-export const COMPARISON = 'COMPARISON PRODUCT';
+export function handleFetchComparisonProduct(productId) {
+  return (dispatch) => {
+    fetchProduct(productId)
+      .then((product) => {
+        dispatch(handleSetComparisonProduct(product));
+        // console.log(product);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+}
 
-export default function handleSetComparisonProduct(product) {
+export default function handleSetComparisonProduct(comparisonDetail) {
   return {
     type: COMPARISON,
-    comparisonDetail: product
+    comparisonDetail
   };
 }
 
