d147d8de586b7a6372aeb3048f79acf6e66ebf68
victoriajquinto <victoriajquinto@gmail.com>
2023-07-07 09:04:15 -0700

    IMPLEMENT A WORKING CAROUSEL WAHOO!!

--- src/components/relatedAndComp/RelatedProducts.jsx
+++ src/components/relatedAndComp/RelatedProducts.jsx
@@ -1,35 +1,33 @@
 import RelatedCard from './RelatedCard.jsx';
 import { Typography, Card, Grid } from '@mui/material';
-import Carousel from 'react-material-ui-carousel';
-
+// import Carousel from 'react-material-ui-carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 export default function RelatedProducts({ relatedProducts }) {
 
   let cardList = relatedProducts.map((productID)=> <RelatedCard productID={productID} key={productID}/>);
-  const sliderItems = 2;
-  // const sliderItems = (relatedProducts.length > 3) ? 3 : relatedProducts.length;
-  // console.log('sliderItems', sliderItems);
-  const items = [];
 
-  for (let i = 0; i < relatedProducts.length; i += sliderItems) {
-    if (i % sliderItems === 0) {
-      <Card key={i.toString()}>
-        <Grid>
-          {items.push(
-            relatedProducts.slice(i, i + sliderItems).map((productID) => {
-              return <RelatedCard productID={productID} key={productID}/>;
-            })
-          )}
-        </Grid>
-      </Card>;
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
-  }
+  };
 
   return (
     <div data-testid='related-products'>
       <Typography component='p' className="title">RELATED PRODUCTS</Typography>
-      <Carousel component='span' id="related-products" width='800px' autoplay={false} interval={null}>
-        <span className='carousel'>{cardList}</span>
+      <Carousel responsive={responsive}>
+        {cardList}
       </Carousel>
     </div>
   );
