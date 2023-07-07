3970a368c177b6794e51cae474bf9a9cfdff9df2
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-07-07 04:20:16 -0500

    Made AnswersList immediately collapsible at all times to improve UX

--- src/components/qAndA/AnswersList.jsx
+++ src/components/qAndA/AnswersList.jsx
@@ -8,25 +8,27 @@ import SeeMore from './SeeMore';
 export default function AnswersList({ answers, length, setLength, isExpanded, setExpanded }) {
 
   return (
-    // max height === 50% of viewport
-    <Box sx={{ maxHeight: '50vh', overflowY: 'auto' }}>
-      { answers
-        .slice(0, length)
-        .map(({ answer_id, body, date, answerer_name, helpfulness, photos, reported }) => (
-          // if answer isn't blank or reported
-          (body.length > 0 && !reported) ? (
-            <Answer key={ answer_id }
-              id={ answer_id }
-              body={ body }
-              date={ getDate(date) }
-              user={ answerer_name }
-              // "seller check" for boldening
-              isSeller={ answerer_name === 'Seller' }
-              helpfulness={ helpfulness }
-              photos={ photos }
-            />
-          ) : (null)
-        )) }
+    <Box>
+      {/* max height === 50% of viewport */}
+      <Box sx={{ maxHeight: '50vh', overflowY: 'auto' }}>
+        { answers
+          .slice(0, length)
+          .map(({ answer_id, body, date, answerer_name, helpfulness, photos, reported }) => (
+            // if answer isn't blank or reported
+            (body.length > 0 && !reported) ? (
+              <Answer key={ answer_id }
+                id={ answer_id }
+                body={ body }
+                date={ getDate(date) }
+                user={ answerer_name }
+                // "seller check" for boldening
+                isSeller={ answerer_name === 'Seller' }
+                helpfulness={ helpfulness }
+                photos={ photos }
+              />
+            ) : (null)
+          )) }
+      </Box>
 
       <Divider sx={{ borderWidth: '1px', margin: '10px 0' }} />
 
